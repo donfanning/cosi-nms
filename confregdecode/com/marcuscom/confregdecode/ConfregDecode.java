482f3c5a7b92da1255fd836668680a0d888aaf7e
jmarcus <>
2001-07-25 22:02:08 +0000

    Upped the version number.

--- confregdecode/com/marcuscom/confregdecode/ConfregDecode.java
+++ confregdecode/com/marcuscom/confregdecode/ConfregDecode.java
@@ -49,7 +49,7 @@ public class ConfregDecode extends Applet implements ActionListener {
     public static final Font DEFAULT_FONT = new Font("SansSerif",Font.PLAIN,12);
     public static final Font BIG_FONT = new Font("SansSerif",Font.BOLD,12);
 
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     static Frame f;
 
