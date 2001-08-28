1c843b744a34f4acd947ae5e5a5ef613f34f2a2f
jmarcus <>
2001-08-28 05:31:15 +0000

    Cleaned up the code some.

--- saaconfig/SAA/SAA_MIB.pm
+++ saaconfig/SAA/SAA_MIB.pm
@@ -4,14 +4,28 @@ use strict;
 require 5.002;
 
 use vars qw(@EXPORT_OK);
-use vars qw($ciscoRttMonMIB $ciscoRttMonObjects $rttMonAppl $rttMonApplVersion $FALSE $TRUE);
+use vars qw(
+  $ciscoRttMonMIB
+  $ciscoRttMonObjects
+  $rttMonAppl
+  $rttMonApplVersion
+  $FALSE
+  $TRUE
+);
 
 use Exporter;
 use Carp;
 
-*import = \&Exporter::import;
+*import    = \&Exporter::import;
 @EXPORT_OK =
-  qw($ciscoRttMonMIB $ciscoRttMonObjects $rttMonAppl $rttMonApplVersion $FALSE $TRUE);
+  qw(
+  $ciscoRttMonMIB
+  $ciscoRttMonObjects
+  $rttMonAppl
+  $rttMonApplVersion
+  $FALSE
+  $TRUE
+);
 
 $ciscoRttMonMIB     = '.1.3.6.1.4.1.9.9.42';
 $ciscoRttMonObjects = $ciscoRttMonMIB . '.1';
@@ -19,7 +33,7 @@ $rttMonAppl         = $ciscoRttMonObjects . '.1';
 $rttMonApplVersion  = $rttMonAppl . '.1';
 
 # These are taken from the SNMPv2-TC definitions.  true is 1 and false is 2.
-$TRUE = 1;
+$TRUE  = 1;
 $FALSE = 2;
 
 1;
