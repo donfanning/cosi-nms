923af08c473cacda793c1d17af91a554f353da78
jmarcus <>
2001-08-28 05:25:06 +0000

    Added support for TruthValue

--- saaconfig/SAA/SAA_MIB.pm
+++ saaconfig/SAA/SAA_MIB.pm
@@ -4,19 +4,23 @@ use strict;
 require 5.002;
 
 use vars qw(@EXPORT_OK);
-use vars qw($ciscoRttMonMIB $ciscoRttMonObjects $rttMonAppl $rttMonApplVersion);
+use vars qw($ciscoRttMonMIB $ciscoRttMonObjects $rttMonAppl $rttMonApplVersion $FALSE $TRUE);
 
 use Exporter;
 use Carp;
 
 *import = \&Exporter::import;
 @EXPORT_OK =
-  qw($ciscoRttMonMIB $ciscoRttMonObjects $rttMonAppl $rttMonApplVersion);
+  qw($ciscoRttMonMIB $ciscoRttMonObjects $rttMonAppl $rttMonApplVersion $FALSE $TRUE);
 
 $ciscoRttMonMIB     = '.1.3.6.1.4.1.9.9.42';
 $ciscoRttMonObjects = $ciscoRttMonMIB . '.1';
 $rttMonAppl         = $ciscoRttMonObjects . '.1';
 $rttMonApplVersion  = $rttMonAppl . '.1';
+
+# These are taken from the SNMPv2-TC definitions.  true is 1 and false is 2.
+$TRUE = 1;
+$FALSE = 2;
 
 1;
 __END__
