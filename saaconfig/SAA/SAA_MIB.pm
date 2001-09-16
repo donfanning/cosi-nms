0535ace639279a0592003adb333abd07f2fc0aed
jmarcus <>
2001-09-16 21:17:02 +0000

    Added createAndWait and other SAA objects.

--- saaconfig/SAA/SAA_MIB.pm
+++ saaconfig/SAA/SAA_MIB.pm
@@ -9,8 +9,13 @@ use vars qw(
   $ciscoRttMonObjects
   $rttMonAppl
   $rttMonApplVersion
+  $rttMonCtrl
+  $rttMonCtrlAdminTable
+  $rttMonCtrlAdminEntry
+  $rttMonCtrlAdminStatus
   $FALSE
   $TRUE
+  $createAndWait
   $SNMP_ERR_NOSUCHNAME
   $SNMP_ERR_V2_IN_V1
   $SNMP_ERR_BAD_VERSION
@@ -26,21 +31,31 @@ use Carp;
   $ciscoRttMonObjects
   $rttMonAppl
   $rttMonApplVersion
+  $rttMonCtrl
+  $rttMonCtrlAdminTable
+  $rttMonCtrlAdminEntry
+  $rttMonCtrlAdminStatus
   $FALSE
   $TRUE
+  $createAndWait
   $SNMP_ERR_NOSUCHNAME
   $SNMP_ERR_V2_IN_V1
   $SNMP_ERR_BAD_VERSION
 );
 
-$ciscoRttMonMIB     = '.1.3.6.1.4.1.9.9.42';
-$ciscoRttMonObjects = $ciscoRttMonMIB . '.1';
-$rttMonAppl         = $ciscoRttMonObjects . '.1';
-$rttMonApplVersion  = $rttMonAppl . '.1';
+$ciscoRttMonMIB        = '.1.3.6.1.4.1.9.9.42';
+$ciscoRttMonObjects    = $ciscoRttMonMIB . '.1';
+$rttMonAppl            = $ciscoRttMonObjects . '.1';
+$rttMonApplVersion     = $rttMonAppl . '.1';
+$rttMonCtrl            = $ciscoRttMonObjects . '.2';
+$rttMonCtrlAdminTable  = $rttMonCtrl . '.1';
+$rttMonCtrlAdminEntry  = $rttMonCtrlAdminTable . '.1';
+$rttMonCtrlAdminStatus = $rttMonCtrlAdminEntry . '.9';
 
 # These are taken from the SNMPv2-TC definitions.  true is 1 and false is 2.
-$TRUE  = 1;
-$FALSE = 2;
+$TRUE          = 1;
+$FALSE         = 2;
+$createAndWait = 5;
 
 # Define some common ucd-snmp return codes.
 $SNMP_ERR_NOSUCHNAME  = 2;
