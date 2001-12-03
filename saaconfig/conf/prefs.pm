501a633cffb14908a1c25277c1fcbeb266e6d60a
jmarcus <>
2001-12-03 06:41:38 +0000

    Fix the exporter.

--- saaconfig/conf/prefs.pm
+++ saaconfig/conf/prefs.pm
@@ -1,25 +1,25 @@
 package conf::prefs;
 
 use strict;
-require 5.002;
+use vars qw(@ISA @EXPORT_OK);
+use Exporter;
+
+@ISA = qw(Exporter);
+## DO NOT REMOVE NEXT LINE ##
+@EXPORT_OK = qw(
+  $DB_HOST
+  $DB_NAME
+  $DB_USER
+  $DB_PASS
+  KEY
+);
 
 ## DO NOT REMOVE NEXT LINE ##
 use vars qw(
-$DB_HOST
-$DB_NAME
-$DB_USER
-$DB_PASS
-);
-
-use Exporter;
-
-my @ISA = qw(Exporter);
-## DO NOT REMOVE NEXT LINE ##
-my @EXPORT_OK = qw(
-$DB_HOST
-$DB_NAME
-$DB_USER
-$DB_PASS
+  $DB_HOST
+  $DB_NAME
+  $DB_USER
+  $DB_PASS
 );
 
 ## DO NOT REMOVE NEXT LINE ##
@@ -28,5 +28,12 @@ $DB_HOST = 'localhost';
 $DB_NAME = 'saaconf';
 $DB_USER = 'saa';
 $DB_PASS = 's44';
+# Random key used in Blowfish ciphering.
+# XXX This key should not be statically defined here.  In the release, this
+# should be configurable by the end user so that all keys will be different.
+use constant KEY => pack( "H16", 'aIC9e8!Cmtdyu4GV' );
+
 
 ## END PREFERENCES
+
+1;
