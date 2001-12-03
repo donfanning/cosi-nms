a9ee5ce59274267abf40895d1d474bb90db9eb3f
jmarcus <>
2001-12-03 05:42:37 +0000

    Initial import into CVS.

---
+++ saaconfig/conf/prefs.pm
@@ -0,0 +1,32 @@
+package conf::prefs;
+
+use strict;
+require 5.002;
+
+## DO NOT REMOVE NEXT LINE ##
+use vars qw(
+$DB_HOST
+$DB_NAME
+$DB_USER
+$DB_PASS
+);
+
+use Exporter;
+
+my @ISA = qw(Exporter);
+## DO NOT REMOVE NEXT LINE ##
+my @EXPORT = qw(
+$DB_HOST
+$DB_NAME
+$DB_USER
+$DB_PASS
+);
+
+## DO NOT REMOVE NEXT LINE ##
+## BEGIN PREFERENCES
+$DB_HOST = 'localhost';
+$DB_NAME = 'saaconf';
+$DB_USER = 'saa';
+$DB_PASS = 's44';
+
+## END PREFERENCES
