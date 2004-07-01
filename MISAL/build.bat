9f25e157ec9ae2193670959d750d7e058f28d034
jmarcus <>
2004-07-01 03:53:34 +0000

    Update build.bat to match the new Makefile.

--- MISAL/build.bat
+++ MISAL/build.bat
@@ -2,46 +2,47 @@ REM
 REM Build batch script for the MISAL project.
 REM $Id$
 
-SET srcdir=.
-SET docdir=%srcdir%\doc\api
-SET package=com.marcuscom.MISAL
-SET package_os=com\marcuscom\MISAL
+SET SRCDIR=.
+SET DOCDIR=%SRCDIR%\doc\api
+SET JARDIR=C:\java\classes
+SET CLASSPATH=.;%JARDIR%\jakarta-oro.jar
+
+SET JAVAC=C:\jdk1.3.1\bin\javac
+SET JAVADOC=C:\jdk1.3.1\bin\javadoc
+SET JAVADOC_FLAGS=-author -version -d %DOCDIR%
+SET JAVAC_FLAGS=-O
+SET JAR=C:\jdk1.3.1\bin\jar
+SET JAR_FLAGS=-cf
+
+SET PACKAGE=com.marcuscom.MISAL
+SET PACKAGE_OS=com\marcuscom\MISAL
 
 SET RM=del
 SET MKDIR=mkdir
 
-SET JAVAC=C:\jdk1.3.1\bin\javac
-SET JAVAC_FLAGS=-O
-SET CLASSPATH=.;C:\classes\PerlTools.jar
-
-SET JAVADOC=C:\jdk1.3.1\bin\javadoc
-SET JAVADOC_FLAGS=-author -version -d %docdir%
-
-SET JAR=C:\jdk1.3.1\bin\jar
-SET JAR_FLAGS=-cf
 
 if "%1"=="clean" goto clean
 
 REM classes:
-%JAVAC% %JAVAC_FLAGS% -classpath %CLASSPATH% %srcdir%\%package_os%\*.java
+%JAVAC% %JAVAC_FLAGS% -classpath %CLASSPATH% %SRCDIR%\%PACKAGE_OS%\*.java
 
 REM jar:
-%JAR% %JAR_FLAGS% %srcdir%\MISAL.jar %srcdir%\%packages_os%\*.class
+%JAR% %JAR_FLAGS% %SRCDIR%\MISAL.jar %SRCDIR%\%PACKAGE_OS%\*.class
 
 REM docs:
-%MKDIR% %docdir%
-%JAVADOC% %JAVADOC_FLAGS% -classpath %CLASSPATH% %package%
+%MKDIR% %DOCDIR%
+%JAVADOC% %JAVADOC_FLAGS% -classpath %CLASSPATH% %PACKAGE%
 
 REM test:
-%JAVAC% %JAVAC_FLAGS% -classpath %CLASSPATH% %srcdir%\MISALTest.java MISALTestIOS.java
+%JAVAC% %JAVAC_FLAGS% -classpath %CLASSPATH% %SRCDIR%\MISALTest.java MISALTestIOS.java
 
 goto end
 
 :clean
-%RM% /q/f %srcdir%\MISAL.jar
-%RM% /q/f %srcdir%\%package_os%\*.class
-%RM% /q/f %docdir%\*.* %docdir%\package-list
-%RM% /q/f/s %docdir%\com
-%RM% /q/f %srcdir%\MISALTest.class %srcdir%\MISALTestIOS.class
+%RM% /q/f %SRCDIR%\MISAL.jar
+%RM% /q/f %SRCDIR%\%PACKAGE_OS%\*.class
+%RM% /q/f %DOCDIR%\*.* %DOCDIR%\package-list
+%RM% /q/f/s %DOCDIR%\com
+%RM% /q/f %SRCDIR%\MISALTest.class %SRCDIR%\MISALTestIOS.class
 
 :end
